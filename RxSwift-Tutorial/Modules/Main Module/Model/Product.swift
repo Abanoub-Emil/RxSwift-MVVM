76fcb8bb74a5f6a8ef4eac70b288543a2f6ecf48
Abanoub Ghaly <abanoub.emil@almtar.com>
2022-03-25 00:34:37 +0200

    RxSwift Examples

---
+++ RxSwift-Tutorial/Modules/Main Module/Model/Product.swift
@@ -0,0 +1,8 @@
+//
+//  Product.swift
+//  RxSwift-Tutorial
+//
+//  Created by Abanoub Emil on 19/03/2022.
+//
+
+import Foundation
---
+++ RxSwift-Tutorial/Modules/Main Module/Presenters/MainPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  MainPresenter.swift
+//  RxSwift-Tutorial
+//
+//  Created by Abanoub Emil on 19/03/2022.
+//
+
+import Foundation
